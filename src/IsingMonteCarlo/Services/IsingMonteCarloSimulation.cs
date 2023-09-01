80ce992d74a5a73190ecfbb1368f49caf05a40b8
Francisco Kim <ratonwasher@gmail.com>
2023-09-01 22:42:42 +0200

    First primitive version of correlation length calc

--- src/IsingMonteCarlo/Services/IsingMonteCarloSimulation.cs
+++ src/IsingMonteCarlo/Services/IsingMonteCarloSimulation.cs
@@ -1,4 +1,6 @@
-﻿using IsingMonteCarlo.Models;
+﻿using System.Numerics;
+
+using IsingMonteCarlo.Models;
 using IsingMonteCarlo.Representations;
 using IsingMonteCarlo.Representations.SpinDynamics;
 
@@ -9,7 +11,10 @@ public sealed class IsingMonteCarloSimulation
     private const int LatticeSizeLowerBound = 3;
 
     private readonly List<List<int>> _neighboursIndices;
-    private SpinUpdateMethod _spinUpdateMethod;
+    private readonly double _q1;
+    private readonly double _q2;
+
+    private SpinUpdateMethod? _spinUpdateMethod;
     private ISpinDynamics _spinDynamics;
 
     public IsingMonteCarloSimulation(
@@ -59,8 +64,20 @@ public sealed class IsingMonteCarloSimulation
         _neighboursIndices = Lattice.NeighboursIndices;
         SpatialVectors = Lattice.SpatialVectors;
 
-        _spinUpdateMethod = SpinUpdateMethod.None;
-        _spinDynamics = new GlauberDynamics(Hamiltonian);
+        _spinUpdateMethod = null;
+       _spinDynamics = new GlauberDynamics(Hamiltonian);
+ 
+        _q1 = 2.0 * Math.PI / Lattice.LatticeLength;
+        _q2 = 2.0 * _q1;
+
+        StructureFactorQ1XContributionFromRealSpinQ = new List<double>();
+        StructureFactorQ1XContributionFromImaginarySpinQ = new List<double>();
+        StructureFactorQ2XContributionFromRealSpinQ = new List<double>();
+        StructureFactorQ2XContributionFromImaginarySpinQ = new List<double>();
+        StructureFactorQ1YContributionFromRealSpinQ = new List<double>();
+        StructureFactorQ1YContributionFromImaginarySpinQ = new List<double>();
+        StructureFactorQ2YContributionFromRealSpinQ = new List<double>();
+        StructureFactorQ2YContributionFromImaginarySpinQ = new List<double>();
     }
 
     public int Dimension { get; }
@@ -74,6 +91,22 @@ public sealed class IsingMonteCarloSimulation
     public IHamiltonian<int> Hamiltonian { get; }
 
     public List<List<int>> SpatialVectors { get; }
+
+    public List<double> StructureFactorQ1XContributionFromRealSpinQ { get; private set; }
+
+    public List<double> StructureFactorQ1XContributionFromImaginarySpinQ { get; private set; }
+
+    public List<double> StructureFactorQ2XContributionFromRealSpinQ { get; private set; }
+
+    public List<double> StructureFactorQ2XContributionFromImaginarySpinQ { get; private set; }
+
+    public List<double> StructureFactorQ1YContributionFromRealSpinQ { get; private set; }
+
+    public List<double> StructureFactorQ1YContributionFromImaginarySpinQ { get; private set; }
+
+    public List<double> StructureFactorQ2YContributionFromRealSpinQ { get; private set; }
+
+    public List<double> StructureFactorQ2YContributionFromImaginarySpinQ { get; private set; }
 
     public void RunMonteCarlo(
         double beta,
@@ -122,5 +155,92 @@ public sealed class IsingMonteCarloSimulation
             _spinDynamics.FlipSpin(beta, j, h, jY);
             ++iterationCount;
         }
+    }
+
+    // Only if coupling strength is symmetrical in all directions
+    public (double InXDirection, double InYDirection) GetCorrelationLength()
+    {
+        MeasureStructureFactorSQ1AndSQ2();
+
+        var sQ1X = (StructureFactorQ1XContributionFromRealSpinQ.Sum() + StructureFactorQ1XContributionFromImaginarySpinQ.Sum())
+                / StructureFactorQ1XContributionFromRealSpinQ.Count;
+        var sQ2X = (StructureFactorQ2XContributionFromRealSpinQ.Sum() + StructureFactorQ2XContributionFromImaginarySpinQ.Sum())
+                / StructureFactorQ2XContributionFromRealSpinQ.Count;
+
+        var sQ1Y = (StructureFactorQ1YContributionFromRealSpinQ.Sum() + StructureFactorQ1YContributionFromImaginarySpinQ.Sum())
+                / StructureFactorQ1YContributionFromRealSpinQ.Count;
+        var sQ2Y = (StructureFactorQ2YContributionFromRealSpinQ.Sum() + StructureFactorQ2YContributionFromImaginarySpinQ.Sum())
+                / StructureFactorQ2YContributionFromRealSpinQ.Count;
+
+        var alternativeCorrelationLengthX = 1.0 / _q1 * Math.Sqrt((sQ1X / sQ2X - 1.0) / (4.0 - sQ1X / sQ2X));
+        var alternativeCorrelationLengthY = 1.0 / _q1 * Math.Sqrt((sQ1Y / sQ2Y - 1.0) / (4.0 - sQ1Y / sQ2Y));
+
+        //return alternativeCorrelationLength
+        //     / Math.Sqrt((1.0 + Dimension) * (3.0 + Dimension) / (8.0 * Dimension));
+        return (alternativeCorrelationLengthX / LatticeLength, alternativeCorrelationLengthY / LatticeLength);
+    }
+
+    private void MeasureStructureFactorSQ1AndSQ2()
+    {
+        var (realQ1X, imaginaryQ1X) = GetFTSpinQInOneDirection(_q1, 0);
+        var (realQ2X, imaginaryQ2X) = GetFTSpinQInOneDirection(_q2, 0);
+
+        var (realQ1Y, imaginaryQ1Y) = GetFTSpinQInOneDirection(_q1, 1);
+        var (realQ2Y, imaginaryQ2Y) = GetFTSpinQInOneDirection(_q2, 1);
+
+        // In the x-direction
+        StructureFactorQ1XContributionFromRealSpinQ.Add(realQ1X * realQ1X);
+        StructureFactorQ1XContributionFromImaginarySpinQ.Add(imaginaryQ1X * imaginaryQ1X);
+        StructureFactorQ2XContributionFromRealSpinQ.Add(realQ2X * realQ2X);
+        StructureFactorQ2XContributionFromImaginarySpinQ.Add(imaginaryQ2X * imaginaryQ2X);
+
+        // In the y-direction
+        StructureFactorQ1YContributionFromRealSpinQ.Add(realQ1Y * realQ1Y);
+        StructureFactorQ1YContributionFromImaginarySpinQ.Add(imaginaryQ1Y * imaginaryQ1Y);
+        StructureFactorQ2YContributionFromRealSpinQ.Add(realQ2Y * realQ2Y);
+        StructureFactorQ2YContributionFromImaginarySpinQ.Add(imaginaryQ2Y * imaginaryQ2Y);
+    }
+
+    private (double Real, double Imaginary) GetFTSpinQInOneDirection(double qX, int componentIndex)
+    {
+        var summationElements = Lattice.Spins.Select(
+                                           (s, i) => s * Complex.Exp(-new Complex(0.0, 1.0) * qX * Lattice.SpatialVectors[i][componentIndex]))
+                                       .ToList();
+
+        var factor = 1.0 / Math.Sqrt(TotalSpinsCount);
+
+        var real = factor * summationElements.Select(element => element.Real).Sum();
+        var imaginary = factor * summationElements.Select(element => element.Imaginary).Sum();
+
+        return (real, imaginary);
+    }
+
+    private (double Real, double Imaginary) GetFTSpinQ(List<double> q)
+    {
+        //if (q.Count != _dimension)
+        //{
+        //    throw new ArgumentException(
+        //        $"The q-vector must be of the same dimension as the lattice, but it has dimension {q.Count}.",
+        //        nameof(q));
+        //}
+
+        var summationElements = Lattice.Spins.Select(
+                                           (s, i) => s
+                                                   * Complex.Exp(
+                                                         -new Complex(0.0, 1.0)
+                                                       * new Complex(
+                                                             scalarProduct(q, Lattice.SpatialVectors[i]),
+                                                             0.0)))
+                                       .ToList();
+
+        var factor = 1.0 / Math.Sqrt(TotalSpinsCount);
+
+        var real = factor * summationElements.Select(element => element.Real).Sum();
+        var imaginary = factor * summationElements.Select(element => element.Real).Sum();
+
+        return (real, imaginary);
+
+        static double scalarProduct(List<double> v1, List<int> v2) =>
+            v1.Zip(v2, (firstV, secondV) => firstV * secondV).Sum();
     }
 }
--- src/IsingSimulator/Program.cs
+++ src/IsingSimulator/Program.cs
@@ -2,6 +2,8 @@
 using IsingMonteCarlo.Models;
 using IsingMonteCarlo.Services;
 
+var loadFile = true;
+//loadFile = false;
 var isSingleRun = true;
 
 var dimension = 2;
@@ -9,41 +11,89 @@ var latticeLength = 100;
 
 var totalSpinsCount = Convert.ToInt32(Math.Pow(latticeLength, dimension));
 
-var initialSpinDownRatio = 0.25;
-var initialSpinConfiguration = SpinConfigurationBuilder.InitialiseLattice(totalSpinsCount,
-                                                                          initialSpinDownRatio,
-                                                                          randomSeed: 17);
+var previousIterationCount = 0;
+var boltzmannTemperature = 0.0;
+var initialSpinConfiguration = new List<int>(totalSpinsCount);
+if (loadFile)
+{
+    var workingDirectory = Environment.CurrentDirectory;
+    var dataDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.Parent.FullName + "\\data";
+    var firstFileName = new DirectoryInfo(dataDirectory).EnumerateFiles()
+                                                        .Select(file => file.FullName)
+                                                        .FirstOrDefault();
+
+    initialSpinConfiguration = LatticeConfigurationSaver.LoadLattice(firstFileName, out boltzmannTemperature, out previousIterationCount);
+}
+else
+{
+    var initialSpinDownRatio = 0.25;
+    initialSpinConfiguration = SpinConfigurationBuilder.InitialiseLattice(totalSpinsCount,
+                                                                              initialSpinDownRatio,
+                                                                              randomSeed: 17);
+    var temperature = 0.0;
+    boltzmannTemperature = 3.0;
+}
+
 var monteCarlo = new IsingMonteCarloSimulation(dimension, latticeLength, initialSpinConfiguration);
 
 // var boltzmannTemperature = 2.269;
-var boltzmannTemperature = 1.8;
 var beta = 1.0 / boltzmannTemperature;
 var j = -1.0;
 var h = 0.0;
+//var iterationLimit = monteCarlo.TotalSpinsCount * 20000;
 var iterationLimit = monteCarlo.TotalSpinsCount * 1000;
-var spinUpdateMethod = SpinUpdateMethod.Glauber;
+var spinUpdateMethod = SpinUpdateMethod.Wolff;
 
 if (isSingleRun)
 {
-    monteCarlo.RunMonteCarlo(beta,
-                             j,
-                             h,
-                             iterationLimit,
-                             spinUpdateMethod,
-                             randomSeed: 17);
+    monteCarlo.RunMonteCarlo(
+        beta,
+        j,
+        h,
+        iterationLimit,
+        spinUpdateMethod);
+    //randomSeed: 17);
 
     var averageMagnetisation = monteCarlo.Hamiltonian.GetAverageMagnetisation(j, h);
 
     Console.WriteLine($"The average magnetisation: {averageMagnetisation}\n");
+    Console.WriteLine($"The correlation length: {boltzmannTemperature},{monteCarlo.GetCorrelationLength()}\n");
+
+    var correlationLengthList = new List<(double InXDirection, double InYDirection)>();
+    for (var k = 0; k < 200; k++)
+    {
+        monteCarlo.RunMonteCarlo(
+            beta,
+            j,
+            h,
+            iterationLimit: 5000,
+            spinUpdateMethod);
+
+        correlationLengthList.Add(monteCarlo.GetCorrelationLength());
+        //Console.WriteLine($"{monteCarlo.GetCorrelationLength().InXDirection},{monteCarlo.GetCorrelationLength().InYDirection},");
+    }
+
+    var correlationLengthListForPrintingX = correlationLengthList.Where(xi => xi.InXDirection is not double.NaN)
+                                                                 .Select(xi => $"{xi.InXDirection}");
+    var correlationLengthListForPrintingY = correlationLengthList.Where(xi => xi.InYDirection is not double.NaN)
+                                                                 .Select(xi => $"{xi.InYDirection}");
+    Console.WriteLine(
+        string.Join(",", correlationLengthListForPrintingX.Concat(correlationLengthListForPrintingY).ToList()));
+
+    LatticeConfigurationSaver.SaveLattice(
+        monteCarlo.Lattice.Spins,
+        boltzmannTemperature,
+        iterationLimit + previousIterationCount,
+        isBinary: true);
 }
 else
 {
     var criticalRangeCount = 8;
 
-    var temperaturesList = new List<double>(3) {0.0001, 0.8, 1.6};
+    var temperaturesList = new List<double>(3) { 0.0001, 0.8, 1.6 };
     var criticalTemperatureRange = Enumerable.Range(0, criticalRangeCount)
                                              .Select(i => 0.5 / criticalRangeCount * i + 2.0);
-    var highTemperatureRange = new List<double>(5) {2.5, 3.0, 3.5, 4.0, 4.5};
+    var highTemperatureRange = new List<double>(5) { 2.5, 3.0, 3.5, 4.0, 4.5 };
 
     temperaturesList.AddRange(criticalTemperatureRange);
     temperaturesList.AddRange(highTemperatureRange);
